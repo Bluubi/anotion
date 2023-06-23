d8501121914c2eadc267639cbb3675f5b4991cfb
BluubiDev <bluubi.dev@gmail.com>
2023-06-23 22:40:50 +0200

    feat: create modal component

---
+++ src/app/shared/ui/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModalComponent {}

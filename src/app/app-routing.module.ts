04153ccf8409fa973963e9da4551143213254477
BluubiDev <bluubi.dev@gmail.com>
2023-06-23 22:45:59 +0200

    feat: create first route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModalComponent } from './shared/ui/modal/modal.component';
+import { AppComponent } from './app.component';
+import { NewPageFormComponent } from './features/new-page-form/new-page-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'new-page',
+    component: NewPageFormComponent,
+    outlet: 'new-page',
+    title: 'New Page',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

372662e81da4c18254c61b0709a9b4b6c3677118
BluubiDev <bluubi.dev@gmail.com>
2023-06-25 02:05:57 +0200

    feat: add button and basic style to modal

--- src/app/shared/ui/modal/modal.component.ts
+++ src/app/shared/ui/modal/modal.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
